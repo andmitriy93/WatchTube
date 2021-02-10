7b70a3ca7eea1fea860fa9262d761c1d43fbf904
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-11 01:26:58 +0600

    added frontend/util/session

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,14 @@
 class Api::UsersController < ApplicationController
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
     def create
         @user = User.new(user_params)
 
@@ -8,6 +18,20 @@ class Api::UsersController < ApplicationController
         else
             render json: @user.errors.full_messages, status: 422
         end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        if @user
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 404
+        end
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+        render :edit
     end
 
 
