f6ff1c62cce543fa334b4a35e5264ae16253cb7f
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-11 07:18:22 +0600

    app.jsx file has been modified

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Provider } from "react-redux";
+import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+
+import SignUpFormContainer from "./session_form/signup_form_container";
+import LogInFormContainer from "./session_form/login_form_container";
 
 const App = () => (
   <div>
-    <h1>Welcome to your journey!</h1>
+    <header>
+      <Link to="/">
+        <h1>YouTuubee</h1>
+      </Link>
+    </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+    </Switch>
   </div>
 );
 
