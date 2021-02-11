04ce8f8b93d3d2eaf81c431006476591b7f953d1
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-11 07:01:39 +0600

    session_form and container are added

--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import SessionForm from './session_form';
-import { login } from '../../actions/session';
+import React from "react";
+import { connect } from "react-redux";
+import SessionForm from "./session_form";
+import { login } from "../../actions/session";
+import { Link } from "react-router-dom";
 
 const mSTP = ({ errors }) => {
-    return {
-        errors: errors.session,
-        formType: 'login',
-    }
-}
+  return {
+    errors: errors.session,
+    formType: "login",
+    navLink: <Link to="/signup">sign up instead</Link>,
+  };
+};
 
-const mDTP = dispatch => {
-    return {
-        processForm: (user) => dispatch(login(user)),
-    };
+const mDTP = (dispatch) => {
+  return {
+    processForm: (user) => dispatch(login(user)),
+  };
 };
 
 export default connect(mSTP, mDTP)(SessionForm);
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
 
 class SessionForm extends React.Component {
-    render() {
-        return null;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        Sign in to continue to YouTuubee
+        <br />
+        <label>
+          Username:
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.update("username")}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.update("password")}
+          />
+        </label>
+        <br />
+        <input type="submit" value={this.props.formType} />
+      </form>
+    );
+  }
 }
 
 export default SessionForm;
