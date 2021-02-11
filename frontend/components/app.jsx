110de07586eaaf54cfdf5d312892d4f64b679ee9
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-12 04:14:44 +0600

    Login and SignUp pages are working

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { AuthRoute, ProtectedRoute } from "../utils/session";
+import { AuthRoute, ProtectedRoute } from "../utils/route_util";
 
 import NavBarContainer from "./nav_bar/nav_bar_container";
 import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
-import Home from "./home/home";
+// import Home from "./home/home";
 
-const App = () => (
+export default () => (
   <div>
     <Route path="/" component={NavBarContainer} />
-    <Route exact path="/" component={Home} />
+    {/* <Route exact path="/" component={Home} /> */}
     <AuthRoute path="/login" component={LogInFormContainer} />
     <AuthRoute path="/signup" component={SignUpFormContainer} />
   </div>
 );
 
-export default App;
