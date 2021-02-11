817a6de5cf63d24616c82dce0606cd93b35d1d5c
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-12 04:03:25 +0600

    Changed app.jsx file

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { Provider } from "react-redux";
-import NavBarContainer from './nav_bar/nav_bar_container';
-import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { AuthRoute, ProtectedRoute } from "../utils/session";
 
+import NavBarContainer from "./nav_bar/nav_bar_container";
 import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
+import Home from "./home/home";
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">
-        <h3>YouTuubee</h3>
-      </Link>
-    </header>
-    <Switch>
-      <Route path="/" component={NavBarContainer} />
-      <Route exact path="/login" component={LogInFormContainer} />
-      <Route exact path="/signup" component={SignUpFormContainer} />
-    </Switch>
+    <Route path="/" component={NavBarContainer} />
+    <Route exact path="/" component={Home} />
+    <AuthRoute path="/login" component={LogInFormContainer} />
+    <AuthRoute path="/signup" component={SignUpFormContainer} />
   </div>
 );
 
