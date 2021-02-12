3fa7b2707c9d270878cbdad7eceec601b5bb5a0b
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-12 17:41:19 +0600

    added nav bar icon

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaAlignRight } from 'react-icons/fa'
+import { FaAlignRight } from "react-icons/fa";
 
 // export default ({ currentUser, logout }) => {
 //   const display = currentUser ? (
@@ -9,7 +9,7 @@ import { FaAlignRight } from 'react-icons/fa'
 //       <button onClick={logout}>Log Out</button>
 //     </div>
 //   ) : (
-//     <div>
+//     <div className="navBar">
 //       <Link to="/signup">Sign Up</Link>
 //       <br/>
 //       <Link to="/login">Log In</Link>
@@ -20,25 +20,32 @@ import { FaAlignRight } from 'react-icons/fa'
 // };
 
 export default class NavBar extends React.Component {
-  state = {
-    toggle:false
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false,
+    };
+    this.Toggle = this.Toggle.bind(this);
   }
-  Toggle = () => {
-    this.setState({toggle:!this.state.toggle})
-  }
+  Toggle() {
+    this.setState({ toggle: !this.state.toggle });
+  };
   render() {
     return (
       <>
-          <div className="navBar">
-                <button onClick={this.Toggle}>
-                    <FaAlignRight />
-                </button>
-                <ul className={this.state.toggle ? "nav-links show-nav" : "nav-links"}>
-                    <li href="#">Home</li>
-                    <li href="#">About us</li>
-                    <li href="#">Contact</li>
-                </ul>
-          </div>
+        <div className="navBar">
+          <button onClick={this.Toggle}>
+            <FaAlignRight />
+          </button>
+          <ul
+            className={this.state.toggle ? "nav-links show-nav" : "nav-links"}
+          >
+            <li href="#">Home</li>
+            <Link to="/signup">Sign Up</Link>
+            <br/>
+            <Link to="/login">Log In</Link>
+          </ul>
+        </div>
       </>
     );
   }
