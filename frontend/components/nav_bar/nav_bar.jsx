9fb40c7499682c0a8a831f3076797bd8b4e58bfd
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-12 16:51:56 +0600

    Working on navBar component

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,18 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaAlignRight } from 'react-icons/fa'
 
-export default ({ currentUser, logout }) => {
-  const display = currentUser ? (
-    <div>
-      <h3>Welcome {(currentUser.username)}</h3>
-      <button onClick={logout}>Log Out</button>
-    </div>
-  ) : (
-    <div>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
-    </div>
-  );
+// export default ({ currentUser, logout }) => {
+//   const display = currentUser ? (
+//     <div>
+//       <h3>Welcome {(currentUser.username)}</h3>
+//       <button onClick={logout}>Log Out</button>
+//     </div>
+//   ) : (
+//     <div>
+//       <Link to="/signup">Sign Up</Link>
+//       <br/>
+//       <Link to="/login">Log In</Link>
+//     </div>
+//   );
 
-  return <header className="nav-bar">{display}</header>;
-};
+//   return <header className="nav-bar">{display}</header>;
+// };
+
+export default class NavBar extends React.Component {
+  state = {
+    toggle:false
+  }
+  Toggle = () => {
+    this.setState({toggle:!this.state.toggle})
+  }
+  render() {
+    return (
+      <>
+          <div className="navBar">
+                <button onClick={this.Toggle}>
+                    <FaAlignRight />
+                </button>
+                <ul className={this.state.toggle ? "nav-links show-nav" : "nav-links"}>
+                    <li href="#">Home</li>
+                    <li href="#">About us</li>
+                    <li href="#">Contact</li>
+                </ul>
+          </div>
+      </>
+    );
+  }
+}
