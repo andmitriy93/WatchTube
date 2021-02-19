fb7c654db5decc649742d36a5e9267ec1d7a0b0f
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-19 08:05:25 +0600

    some change

--- frontend/components/video/video_index_item.jsx
+++ frontend/components/video/video_index_item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoIndexItem = props => {
     return (
         <div className="vid">
-            <video className="vid-player" controls src={props.video.video_url} width="350"/>
+            <video className="vid-player" controls src={props.video.video_url} width="350" />
             <p className="desc">{props.video.id} {props.video.title}</p>
         </div>
     )
