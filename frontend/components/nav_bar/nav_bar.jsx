f7bd9c5192b7093e986f58621021530da7ed97b4
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-13 01:02:02 +0600

    working on CSS styling homepage navbar and searchbar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SearchBar from '../search/search'
+import SearchBar from "../search/search";
 
 export default ({ currentUser, logout }) => {
   const display = currentUser ? (
-    <div>
-        {/* <h3 className="welcome">Welcome {currentUser.username}</h3> */}
+    <div className="nav-curr-user">
+      <SearchBar />
+      <h3 className="welcome">Welcome {currentUser.username}</h3>
       <button onClick={logout}>Log Out</button>
     </div>
   ) : (
-    <div className='nav'>
+    <div className="nav">
       <div className="logo">
         {/* <span>Menu</span> */}
-        <Link className="img" to="/" ><img src={window.YoutubeURL} /></Link>
+        <Link className="img" to="/">
+          <img src={window.YoutubeURL} />
+        </Link>
       </div>
       <SearchBar />
       <div>
-      <Link className="btn" to="/login">Sign In</Link>
+        <Link className="btn" to="/login">Sign In</Link>
       </div>
-      
     </div>
   );
 
