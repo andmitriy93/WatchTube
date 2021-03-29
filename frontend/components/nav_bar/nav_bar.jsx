b66252d0720ac0faef46d3d01ee467ae33b8bc95
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-30 00:53:44 +0600

    Delete old navbar code

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -2,35 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../search/search";
 
-// export default ({ currentUser, logout }) => {
-//   const display = currentUser ? (
-//     <div className="nav-curr-user">
-//       <h3 className="welcome">Welcome {currentUser.username}</h3>
-//       <SearchBar />
-//       <div className="nav-profile-menu">
-//         <i class="fas fa-user-circle"></i>
-//       </div>
-//       <button className="logout-btn" onClick={logout}>Log Out</button>
-//     </div>
-//   ) : (
-//     <div className="nav-guest">
-//         <Link className="img-youtube" to="/">
-//           <img src={window.YoutubeURL} />
-//         </Link>
-//       <SearchBar />
-//       <div className="nav-profile-menu">
-//         <i className="fas fa-user-circle"></i>
-//       </div>
-//       <Link className="navbar-login-btn" to="/login">Sign In</Link>
-//     </div>
-//   );
-
-//   return (
-//     <header>
-//       <div>{display}</div>
-//     </header>
-//   );
-// };
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -82,24 +53,7 @@ class Navbar extends React.Component {
       return (
         <div className='nav-guest'>
           <div></div>
-          {/* <div className="nav-logo-wrapper">
-            <Link className='img-youtube' to='/'>
-              <img src={window.YoutubeURL} />
-              <i class="fab fa-youtube"></i>
-              <span className="nav-logo-text">WatchTube</span>
-            </Link>
-          </div> */}
           <SearchBar />
-          {/* <div className='nav-profile-menu'>
-            <i onClick={this.showMenu} className='fas fa-user-circle'></i>
-            {this.state.showMenu ? (
-              <div className='navbar-menu'>
-                <Link className='navbar-login-btn' to='/login'>
-                <i class="fas fa-user-circle signin"></i> Sign In
-                </Link>
-              </div>
-            ) : null}
-          </div> */}
           <div className='navbar-signin'>
             <Link className='navbar-login-btn' to='/login'>
               <i class="fas fa-user-circle signin navbar"></i> Sign In
