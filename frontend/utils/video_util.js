15f3af621ecf2f3e776e40f3d267053634abffdf
Dmitrii An <an.dmitriy93@gmail.com>
2021-04-04 00:35:34 +0600

    Fix issue with search feature add explicit return in ajax call util file

--- frontend/utils/video_util.js
+++ frontend/utils/video_util.js
@@ -1,18 +1,20 @@
-export const fetchVideos = () =>
-  $.ajax({
+export const fetchVideos = () => {
+  return $.ajax({
     method: "GET",
     url: "/api/videos",
   });
+};
 
-export const fetchVideo = (videoId) =>
-  $.ajax({
+export const fetchVideo = (videoId) => {
+  return $.ajax({
     method: "GET",
     url: `/api/videos/${videoId}`,
     data: { videoId },
   });
+};
 
 export const searchVideos = (videoTitle) => {
-  $.ajax({
+  return $.ajax({
     method: "GET",
     url: "/api/videos/search",
     data: { videoTitle },
